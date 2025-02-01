a8dd1eaa58d3d6eb183df23f8deebc3922b97597
liammota3d <motadaniel246@gmail.com>
2025-01-31 21:09:52 -0300

    Add files via upload

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "AVATAR-My project (5)-0.1";
+const contentToCache = [
+    "Build/AVATAR_3D.loader.js",
+    "Build/AVATAR_3D.framework.js",
+    "Build/AVATAR_3D.data",
+    "Build/AVATAR_3D.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
